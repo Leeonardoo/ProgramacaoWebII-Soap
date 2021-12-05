2048a8cd05b95ee97020f7adfcbcd101cba05b45
Daniel Busarello <62729531+DanielBusarello@users.noreply.github.com>
2021-12-05 16:01:34 -0300

    Validações

--- src/main/kotlin/br/furb/progweb/soap/endpoint/UserEndpoint.kt
+++ src/main/kotlin/br/furb/progweb/soap/endpoint/UserEndpoint.kt
@@ -1,8 +1,6 @@
 package br.furb.progweb.soap.endpoint
 
-import br.furb.progweb.soap.GetUserRequest
-import br.furb.progweb.soap.GetUserResponse
-import br.furb.progweb.soap.NAMESPACE_URI
+import br.furb.progweb.soap.*
 import br.furb.progweb.soap.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ws.server.endpoint.annotation.Endpoint
@@ -12,12 +10,19 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload
 
 @Endpoint
 class UserEndpoint @Autowired constructor(private val userRepository: UserRepository) {
-
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserRequest")
     @ResponsePayload
     fun getUser(@RequestPayload request: GetUserRequest): GetUserResponse {
         return GetUserResponse().apply {
             user = userRepository.findUser(request.id)
+        }
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "createUserRequest")
+    @ResponsePayload
+    fun createUser(@RequestPayload request: CreateUserRequest): GetUserResponse {
+        return GetUserResponse().apply {
+            user = userRepository.addUser(request)
         }
     }
 }
